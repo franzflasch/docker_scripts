3f3aca09171c89670be3df9f8551e5c173f46c97
Flasch Franz <ffl@mission-embedded.com>
2018-12-04 16:59:23 +0100

    Added support for sharing ssh-agent socket with host, some minor improvements

--- docker_start_with_x.sh
+++ docker_start_with_x.sh
@@ -8,6 +8,13 @@ print_usage() {
    echo "./docker_start_with_x.sh -i ubuntu18_04_3dprinting:1 -u franz -o \"-v /home/franz/:/working_dir --device /dev/ttyUSB0\""
    echo "use this as optional_docker_arf to enable support for tun/tap devices in docker:"
    echo "--cap-add=NET_ADMIN --device /dev/net/tun"
+   echo ""
+   echo "Options:"
+   echo "-u       username within docker image"
+   echo "-i       docker image to take"
+   echo "-o       optional docker arguments e.g. \"-v<some-dir>:<some-dir>\""
+   echo "-r       auto remove docker container after exit"
+   echo "--ssh    map SSH agent socket into container, so that host ssh-keys can be used within docker container"
    exit 1
 }
 
@@ -37,6 +44,11 @@ do
       REMOVE_FLAG="--rm"
       shift # past argument
       ;;
+      --ssh)
+      ssh_auth_sock_file=$(dirname $SSH_AUTH_SOCK)/$(basename $SSH_AUTH_SOCK)
+      DOCKER_SSH_AUTH_SOCK_OPTS="-v $ssh_auth_sock_file:$ssh_auth_sock_file -e SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
+      shift # past argument
+      ;;
       -h|--help)
       print_usage
       ;;
@@ -54,16 +66,32 @@ echo "Starting image $DOCKER_IMAGE with the following options:"
 echo DOCKER_USER      = "${DOCKER_USER}"
 echo OPT_DOCKER_ARGS  = "${OPT_DOCKER_ARGS}"
 echo REMOVE_FLAG      = "${REMOVE_FLAG}"
+echo DOCKER_SSH_AUTH_SOCK_OPTS = "${DOCKER_SSH_AUTH_SOCK_OPTS}"
+
+## Graphical SECTION
+# -e DISPLAY=$DISPLAY \
+# -v /tmp/.X11-unix:/tmp/.X11-unix \
+# --device /dev/dri \
+
+## Sound support
+# --group-add $(getent group audio | cut -d: -f3) \
+# --device /dev/snd \
+# -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+
+## This is needed by some applications e.g. gnome-terminal:
+# -v /run/dbus/:/run/dbus/ \
+# -v /dev/shm:/dev/shm \
 
 docker run -ti $REMOVE_FLAG\
            --user=$DOCKER_USER \
            -e DISPLAY=$DISPLAY \
            -v /tmp/.X11-unix:/tmp/.X11-unix \
+           --device /dev/dri \
            --group-add $(getent group audio | cut -d: -f3) \
            --device /dev/snd \
-           --device /dev/dri \
            -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
            -v /run/dbus/:/run/dbus/ \
            -v /dev/shm:/dev/shm \
+           $DOCKER_SSH_AUTH_SOCK_OPTS \
            $OPT_DOCKER_ARGS \
            $DOCKER_IMAGE
